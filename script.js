048b259524f4a95aa455b5295dc71f815d8bc66d
alexjakstas <alexsas.jakstas@gmail.com>
2025-12-17 21:04:05 +0200

    bmi

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form");
+const results = document.querySelector("#results");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+
+    if(!height || height <= 0 || isNaN(height)){
+        results.textContent = "Please provide a valid height";
+
+    } else if(!weight || weight <= 0 || isNaN(weight)){
+        results.textContent = "Please provide a valid weight";
+
+    } else{
+        const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+        results.innerHTML = `<span> Your BMI is: ${bmi}`;
+    }
+})
